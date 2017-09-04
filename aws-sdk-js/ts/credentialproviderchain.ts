b23d891dc4e419dcd0f2757b1a96ecb0d1307c7e
Sam Noreaksey <sam.noreaksey@outlook.com>
2017-09-05 00:26:25 +1000

    Upload past project.

---
+++ aws-sdk-js/ts/credentialproviderchain.ts
@@ -0,0 +1,11 @@
+import {CredentialProviderChain} from '../lib/credentials/credential_provider_chain';
+import {Credentials} from "../lib/credentials";
+
+const providerChain = new CredentialProviderChain([]);
+
+providerChain.resolvePromise().then(
+    (creds: Credentials) => {},
+    err => {}
+);
+
+providerChain.resolve(() => {});
